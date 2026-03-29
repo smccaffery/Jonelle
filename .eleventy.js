e1e85bef8933ebefc5743cda958d17c71e5dab5d
Sarah <smccaffery@gmail.com>
2026-03-29 12:42:34 -0400

    Revert "."
    This reverts commit f4047fef1aa29289724677267ceb301ef5bc2b0e.

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,16 @@
-const { DateTime } = require("luxon");
-
 module.exports = function(eleventyConfig) {
   // Passthrough copy for static assets. Eleventy will copy these
   // files/folders to the output directory automatically.
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("images");
+
   // Passthrough for the webmanifest
   eleventyConfig.addPassthroughCopy("site.webmanifest");
-
-  // Add a collection for blog posts
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("blog/posts/*.md").sort((a, b) => {
-      return b.date - a.date; // Sort in reverse chronological order
-    });
-  });
-
-  // Add a date filter using Luxon for consistent date formatting
-  eleventyConfig.addFilter("date", (dateObj, format) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
-  });
 
   return {
     dir: {
       input: ".",
-      output: "_site",
-      layouts: "_includes/layouts" // Specify the layouts directory
+      output: "_site"
     },
     // Set Nunjucks as the default template engine for HTML files
     htmlTemplateEngine: "njk",
